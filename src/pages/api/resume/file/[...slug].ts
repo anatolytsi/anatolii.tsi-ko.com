d49e0e39d0fecaa086a10583c4adf0d25f92d909
Anatolii Tsirkunenko <snipl33t@gmail.com>
2023-05-18 19:30:20 +0200

    Separate image path extraction into functions

--- src/pages/api/resume/file/[...slug].ts
+++ src/pages/api/resume/file/[...slug].ts
@@ -4,9 +4,17 @@ import { convertToAbsolutePath } from "@/lib/parse-form";
 import { isArray } from "lodash";
 import { IMAGES_URL } from ".";
 
+export const getImagePath = (imageName: string) => {
+  return convertToAbsolutePath(`${IMAGES_URL}/${imageName}`);
+}
+
+export const getImageFromName = (imageName: string) => {
+  return fs.readFileSync(getImagePath(imageName).replace('..', '.'));
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const imagePath = isArray(req.query.slug) ? req.query.slug.join('/') : req.query.slug!;
-  const imageBuffer = fs.readFileSync(convertToAbsolutePath(`${IMAGES_URL}/${imagePath}`).replace('..', '.'));
+  const imageBuffer = getImageFromName(imagePath);
   res.setHeader("Content-Type", "image");
   return res.send(imageBuffer);
 }
