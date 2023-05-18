20856c49c04368ab1c5a3e850618713314f6b810
Anatolii Tsirkunenko <snipl33t@gmail.com>
2023-05-18 19:37:17 +0200

    Rework pdf generation

--- src/lib/pdfHelper.ts
+++ src/lib/pdfHelper.ts
@@ -1,40 +1,66 @@
 import { renderToStaticMarkup } from 'react-dom/server';
-import pdf, { CreateOptions } from 'html-pdf';
+import puppeteer from 'puppeteer';
+import { getImageFromName } from '@/pages/api/resume/file/[...slug]';
+import { API_URL } from '@/pages/api/resume/file';
 
-const componentToPDFBuffer = (component: any) => {
-    return new Promise((resolve, reject) => {
-        let html = renderToStaticMarkup(component);
-        // html = html.replace('h3', 'p');
-        // html = html.replace('h4', 'p');
-        // html = html.replace('h5', 'p');
-        // html = html.replace('div', 'p');
-        // html = html.replace('input', 'p');
-        // html = html.replace('<p', '<p style="font-size: 8px;"');
-        
-        // html = html.replace('<p', '<p style="font-size: 14px;"');
-        // html = html.replace('<p', '<p style="font-size: 14px;"');
-        // html = html.replace('<p', '<p style="font-size: 14px;"');
-        // html = html.replace('<p', '<p style="font-size: 14px;"');
+const IMG_RE = /<img(.|\n)*src="([^"]*)/g;
+const EXT_RE = /(?:\.([^.]+))?$/;
 
-        const options: CreateOptions = {
-            format: 'A4',
-            orientation: 'portrait',
-            border: '10mm',
-            footer: {
-                height: '10mm',
-            },
-            type: 'pdf',
-            timeout: 30000,
-        };
-
-        const buffer = pdf.create(html, options).toBuffer((err: any, buffer: any) => {
-            if (err) {
-                return reject(err);
+const componentToPDFBuffer = async (component: any) => {
+    let html = renderToStaticMarkup(component);
+    let match;
+    do {
+        match = IMG_RE.exec(html);
+        if (match) {
+            const imageName = match[2].replace(`${API_URL}/`, '');
+            const imageBuffer = getImageFromName(imageName);
+            let extension = 'jpeg';
+            if (imageName.includes('.')) {
+                let m = EXT_RE.exec(imageName);
+                extension = m ? m[1] : 'jpeg';
             }
-        
-            return resolve(buffer);
-        });
+            const imageForPdf = `data:image/${extension};base64,${imageBuffer.toString('base64')}`;
+            html.replace(match[2], imageForPdf);
+        }
+    } while (match);
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/chromium-browser',
+        headless: 'new',
+        args: [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--allow-file-access-from-files',
+            '--enable-local-file-accesses'
+        ]
     });
+
+    const page = await browser.newPage();
+    await page.setContent(html, {
+        waitUntil: [
+            'domcontentloaded', 
+            'networkidle0', 
+            'load', 
+            'networkidle2'
+        ], 
+        timeout: 30000
+    });
+    await page.emulateMediaType('screen');
+    const pdf = await page.pdf({
+        format: 'A4',
+        scale: 0.7,
+        margin: {
+            top: '20px',
+            bottom: '20px',
+            left: '20px',
+            right: '20px',
+        },
+        printBackground: true
+    });
+    await browser.close();
+    return pdf;
 }
 
 export default {
