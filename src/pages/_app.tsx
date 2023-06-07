acd3e559f1d454d09cfb7ee0992d6137838b2b76
Anatolii Tsirkunenko <snipl33t@gmail.com>
2023-06-07 18:47:18 +0200

    Add google metrics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react';
 import Navbar from '@/components/layout'
+import Script from 'next/script';
 
 export default function App({ 
   Component, 
@@ -11,6 +12,22 @@ export default function App({
     <SessionProvider session={session}>
       <Navbar/>
       <Component {...pageProps} />
+      <div className="container">
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <Script
+          src={`"https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}"`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </div>
     </SessionProvider>
   )
 }
