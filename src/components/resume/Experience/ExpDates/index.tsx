49933649c2ef0069b64e0cc78e0b0533a42205da
Anatolii Tsirkunenko <snipl33t@gmail.com>
2023-06-08 17:46:37 +0200

    Add time calculation between the dates and update dates styling

--- src/components/resume/Experience/ExpDates/index.tsx
+++ src/components/resume/Experience/ExpDates/index.tsx
@@ -31,6 +31,26 @@ export const ExpDates = ({ isEditing,
                            setter,
                            exp,
                            forExport=false }: ICommonExperienceProps) => {
+    const getTimePeriod = (startDate: any, endDate: any) => {
+        let dateDifMs = endDate - startDate;
+        let passedDate = new Date(dateDifMs);
+        let years = Math.abs(passedDate.getUTCFullYear() - 1970);
+        let months = passedDate.getUTCMonth();
+        let output = '';
+        if (years) {
+            output = `${years} year${years === 1 ? '' : 's'}`;
+            output += months ? ` and ${months} month${months === 1 ? '' : 's'}` : '';
+        } else {
+            output = months ? `${months} month${months === 1 ? '' : 's'}` : '';
+        }
+
+        if (!years && !months) {
+            let days = passedDate.getUTCDate();
+            output = days ? `${days} day${days === 1 ? '' : 's'}` : '';
+        }
+
+        return output;
+    }
 
     const updateDate = (datekey: 'startDate' | 'endDate', date: Date) => {
         let dateMs: number = date.getTime();
@@ -55,33 +75,38 @@ export const ExpDates = ({ isEditing,
     endDateClass = exp?.endDate ? endDateClass : `${endDateClass} ${styles.current}`
 
     return (
-        <div className={styles.dates}>
-            <DatePicker
-                wrapperClassName={styles.datePicker}
-                selected={new Date(exp?.startDate ?? 0)}
-                className={isEditing ? styles.editingStartDate : ''}
-                readOnly={!isEditing}
-                onChange={(date: Date) => updateDate('startDate', date)}
-                dateFormat={DATES_FORMAT}
-            />
-            <span className={styles.datesDash}>-</span>
-            <DatePicker
-                wrapperClassName={endDateClass}
-                selected={exp?.endDate ? new Date(exp.endDate) : null}
-                className={isEditing ? styles.editingEndDate : ''}
-                readOnly={!isEditing}
-                onChange={(date: Date) => updateDate('endDate', date)}
-                dateFormat={DATES_FORMAT}
-                placeholderText="Current"
-            />
-            {isEditing ? (
-                <button
-                    className={styles.setCurrent}
-                    onClick={setTillCurrentDate}
-                >
-                    Set current
-                </button>
-            ) : ('')}
+        <div className={styles.datesWithTime}>
+            <p className={styles.time}>
+                {getTimePeriod(exp?.startDate, exp?.endDate ? exp?.endDate : (new Date()).getTime())}
+            </p>
+            <div className={styles.dates}>
+                <DatePicker
+                    wrapperClassName={styles.datePicker}
+                    selected={new Date(exp?.startDate ?? 0)}
+                    className={isEditing ? styles.editingStartDate : ''}
+                    readOnly={!isEditing}
+                    onChange={(date: Date) => updateDate('startDate', date)}
+                    dateFormat={DATES_FORMAT}
+                />
+                <span className={styles.datesDash}>-</span>
+                <DatePicker
+                    wrapperClassName={endDateClass}
+                    selected={exp?.endDate ? new Date(exp.endDate) : null}
+                    className={isEditing ? styles.editingEndDate : ''}
+                    readOnly={!isEditing}
+                    onChange={(date: Date) => updateDate('endDate', date)}
+                    dateFormat={DATES_FORMAT}
+                    placeholderText="Current"
+                />
+                {isEditing ? (
+                    <button
+                        className={styles.setCurrent}
+                        onClick={setTillCurrentDate}
+                    >
+                        Set current
+                    </button>
+                ) : ('')}
+            </div>
         </div>
     );
 }
