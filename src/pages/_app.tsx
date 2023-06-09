bb0cf7ddbb04918c9a157a767af2f465607f3018
Anatolii Tsirkunenko <snipl33t@gmail.com>
2023-06-09 09:32:52 +0200

    Move analytics tag to very top of application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,25 +9,25 @@ export default function App({
   pageProps: { session, ...pageProps }
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Navbar/>
-      <Component {...pageProps} />
-      <div className="container">
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-7H48CHEZ6F"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
+    <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7H48CHEZ6F"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-            gtag('config', 'G-7H48CHEZ6F');
-          `}
-        </Script>
-      </div>
-    </SessionProvider>
+          gtag('config', 'G-7H48CHEZ6F');
+        `}
+      </Script>
+      <SessionProvider session={session}>
+        <Navbar/>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
