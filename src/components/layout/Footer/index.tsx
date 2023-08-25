3b762a66561e709b4b70bd928a0d0fe64f863bee
Anatolii Tsirkunenko <snipl33t@gmail.com>
2023-08-25 21:49:23 +0200

    Mordernised footer

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,12 +1,28 @@
 import styles from './Footer.module.scss';
 
-//#f5f5f7
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
 export const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div>
+                <div className={styles.links}>
+                    <a className={styles.link} href='https://github.com/anatolytsi/' target='_blank'>
+                        <FontAwesomeIcon icon={faGithub} size='lg' />
+                    </a>
+                    <span className={styles.divider}>/</span>
+                    <a className={styles.link} href='https://linkedin.com/in/anatolytsi/' target='_blank'>
+                        <FontAwesomeIcon icon={faLinkedinIn} size='lg' />
+                    </a>
+                    <span className={styles.divider}>/</span>
+                    <a className={styles.link} href='mailto:anatolii@tsi-ko.com' target='_blank'>
+                        <FontAwesomeIcon icon={faEnvelope} size='lg' />
+                    </a>
+                </div>
                 <p>
-                    &copy;{new Date().getFullYear()} 
+                    Copyright &copy; {new Date().getFullYear()} 
                     <a href="https://www.linkedin.com/in/anatolytsi/"
                        target="_blank">
                         Anatolii Tsirkunenko
