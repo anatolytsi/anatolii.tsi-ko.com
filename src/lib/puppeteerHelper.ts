98399f33449136b3681a6a0db3040378d4c91eb7
Anatolii Tsirkunenko <snipl33t@gmail.com>
2024-08-18 23:04:35 +0200

    add custom fonts to pdf

--- src/lib/puppeteerHelper.ts
+++ src/lib/puppeteerHelper.ts
@@ -1,9 +1,42 @@
+import fs from "fs";
 import { renderToStaticMarkup } from 'react-dom/server';
 import puppeteer, { Page, Viewport } from 'puppeteer';
 import { getImageFromName } from '@/pages/api/resume/file/[...slug]';
 
 const IMG_RE = /<img(.|\n)*src="([^"]*)/g;
 const EXT_RE = /(?:\.([^.]+))?$/;
+
+const PATH_FONT_1 = '../styles/fonts/tw_cen_mt.ttf';
+const PATH_FONT_2 = '../styles/fonts/tw_cen_mt_bold.ttf';
+const HEAD_END_HTML = '</head>';
+const CUSTOM_FONT_CLASS_NAME = 'customFont';
+const SRC_TO_REPLACE_STR = 'sourceToReplace';
+const STYLE_CUSTOM_FONT = `
+<style>
+.${CUSTOM_FONT_CLASS_NAME} {
+    @font-face {
+        font-family: 'Tw Cen MT';
+        src: ${SRC_TO_REPLACE_STR};
+        font-weight: normal;
+        font-style: normal;
+    }
+}
+</style>`;
+const MAIN_START_HTML = '<main>';
+const MAIN_CUSTOM_FONT = `<main class="${CUSTOM_FONT_CLASS_NAME}">`;
+
+const addCustomFont = (html: string): string => {
+    html = html.replace(MAIN_START_HTML, MAIN_CUSTOM_FONT);
+
+    let font1 = fs.readFileSync(PATH_FONT_1);
+    let font2 = fs.readFileSync(PATH_FONT_2);
+    let fontStyle = STYLE_CUSTOM_FONT.replace(SRC_TO_REPLACE_STR, 
+        `url(data:application/font-woff2;charset=utf-8;base64,${font1.toString('base64')}),
+        url(data:application/font-woff;charset=utf-8;base64,${font2.toString('base64')})`)
+    html = html.replace(HEAD_END_HTML, `${fontStyle}\n${HEAD_END_HTML}`);
+
+    return html;
+}
 
 export const preparePage = async (component: any, imageUrl: string, imagesPath: string, callback: (page: Page) => Promise<void>,
                                   viewport: Viewport = {width: 0, height: 0}) => {
@@ -24,6 +57,7 @@ export const preparePage = async (component: any, imageUrl: string, imagesPath: 
             html = html.replace(match[2], imageForPdf);
         }
     } while (match);
+    html = addCustomFont(html);
     const browser = await puppeteer.launch({
         executablePath: '/usr/bin/chromium-browser',
         ignoreHTTPSErrors :true,
